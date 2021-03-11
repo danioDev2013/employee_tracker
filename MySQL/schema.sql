a7825c99401e2d2240ec791f277347a8ec155d5d
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-11 11:27:36 -0800

    created sql files, started schema file commit

---
+++ MySQL/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employeesDB;
+
+CREATE DATABASE employeesDB;
+
+USE employeesDB;
+
+CREATE TABLE department (
+
+);
+
+CREATE TABLE roles (
+
+);
+
+CREATE TABLE employee {
+
+};
