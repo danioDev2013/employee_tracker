7c6da1c47da8b26377aafc3176d9d33a46d6e96d
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-11 10:14:37 -0800

    created connection and connection info

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+//connection information for the sql database
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: '',
+    database: 'employeesDB',
+  });
+  
 
 //functions
 //prompts users
