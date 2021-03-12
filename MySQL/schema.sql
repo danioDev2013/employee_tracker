92bd0dcc48c3116515f7f580ffa7440cf0a951fd
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-11 21:16:25 -0800

    updated insert department, roles, and employees commit

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -1,42 +1,42 @@
 -- Drop is employeesDB exists
 DROP DATABASE IF EXISTS employeesDB;
 
---Create employees database
+-- Create employees database
 CREATE DATABASE employeesDB;
 USE employeesDB;
 
---Create department table
+-- Create department table
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    --holds dept name
+    -- holds dept name
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 -- Creates roles table
 CREATE TABLE roles (
-    --roles id
+    -- roles id
     roles_id INT NOT NULL AUTO_INCREMENT,
-    --holds role title
+    -- holds role title
     title VARCHAR(30) NOT NULL,
-    --holds role salary 
+    -- holds role salary 
     salary DECIMAL(9,2),
-    --holds reference to department role belongs to
+    -- holds reference to department role belongs to
     department_id INT,
-    PRIMARY KEY (r_id)
+    PRIMARY KEY (roles_id)
 );
 
---Creates employee table
-CREATE TABLE employee {
+-- Creates employee table
+CREATE TABLE employee (
     -- id
-    id INT NOT NULL AUTO_INCREMENT,
+    empl_id INT NOT NULL AUTO_INCREMENT,
     -- holds employee first name
-    first_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
     -- holds employees last name
-    last_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30),
     -- holds reference to role employee has
     role_id INT,
     --  holds reference to another employee that manages the employee being Created
     manager_id INT,
     PRIMARY KEY (empl_id)
-};
+    );
--- MySQL/seed.sql
+++ MySQL/seed.sql
@@ -1,18 +1,40 @@
 --Seeds for SQL table
 USE employeesDB;
 
-INSERT INTO department(dept_name)
+INSERT INTO department (dept_name)
 VALUES ('Management'),
 ('Kennel Staff'),
 ('Customer Service'),
 ('Groomers'),
 ('Custodial');
 
-INSERT INTO roles(title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ('CEO', 500000, 1),
-('Kennel Attendant', 30000, 2),
+('Manager', 50000, 1),
+('Kennel Attendant Lead', 30000, 2),
+('Kennel Assistant', 24000, 2),
 ('Front Desk Staff', 24000, 3),
 ('Groomer Lead', 40000, 4),
 ('Groomer', 30000, 4),
 ('Grooming Assistant', 24000, 4),
+('Dog Bather', 20000, 4),
 ('Kennel Cleaner', 25000, 5),
+('Dog Play Area Cleaner', 2500, 5),
+('Accountant', 40000, 6);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Hubert','Farnsworth', 1, NULL),
+('Turanga', 'Leela', 2, 1),
+('Zapp', 'Brannigan', 3, 2),
+('Kif', 'Kroker', 4, 3),
+('Amy', 'Wong', 5, 2),
+('John', 'Zoidberg', 6, 2),
+('Robot', 'Santa', 7, 6),
+('Lord', 'Nibbler', 8, 6),
+('Philip', 'Fry', 9, 4),
+('Bender', 'Rodriguez', 10, 2),
+('Brain', 'Slugs', 11, 2),
+('Hermes', 'Conrad', 12, 1);
+
+
+
