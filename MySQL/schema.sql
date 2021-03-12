2771a5118ca63a7a29004efe94ffaa19afc263b2
Danielle Orley <danielle.m.orley1993@gmail.com>
2021-03-11 16:53:59 -0800

    final schema commit

--- MySQL/schema.sql
+++ MySQL/schema.sql
@@ -5,13 +5,21 @@ CREATE DATABASE employeesDB;
 USE employeesDB;
 
 CREATE TABLE department (
-
+    id INT NOT NULL AUTO_INCREMENT primary key,
+    dept_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-
+    r_id INT NOT NULL AUTO_INCREMENT primary key,
+    role_name VARCHAR(30),
+    role_salary DECIMAL(9,2),
+    department_id INT
 );
 
 CREATE TABLE employee {
-
+    empl_id INT NOT NULL AUTO_INCREMENT primary key,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
 };
